18c2b775e152e2ee79524936ce15640384b5d2d5
MajoLed <marysaya12@gmail.com>
2026-09-06 20:41:30 -0500

    ADT's hechos

---
+++ src/main/java/SecuenciaLista.java
@@ -0,0 +1,36 @@
+public class SecuenciaLista implements SecuenciaEnteros {
+    private Nodo cabeza;
+    private int tamano;
+
+    public SecuenciaLista(int[] valores) {
+        this.tamano = valores.length;
+    }
+
+    public int obtener(int i) {
+        Nodo actual = cabeza;
+        for (int k = 0; k < i; k++) {
+            actual = actual.siguiente;
+        }
+        return actual.valor;
+    }
+
+    public int tamano() {
+        return tamano;
+    }
+
+    // Clase nodo
+    private class Nodo {
+        int valor;
+        Nodo siguiente;
+        Nodo(int valor) { this.valor = valor; }
+    }
+
+    @Override
+    public String toString() {
+        return "SecuenciaLista{" +
+                "cabeza=" + cabeza +
+                ", tamano=" + tamano +
+                '}';
+    }
+
+}
